88daa0243a485d09853f35667db0839672132c8a
TichonovRoman <romikt@inbox.ru>
2021-12-22 13:34:46 +0300

    1. Учебный проект

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Reting";
+
+function Hello() {
+    return (
+        alert('Hello')
+    )
+}
+
+//Hello()
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    console.log('App rendering')
+    return (
+        <div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
+            <Rating/>
+        </div>
+
+    );
 }
+
+function AppTitle() {
+    console.log('AppTitle rendering')
+    return <>This is App component</>
+
+}
+
 
 export default App;
---
+++ src/components/Rating/Reting.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export function Rating() {
+    console.log('Rating rendering')
+    return (
+        <>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </>
+    )
+}
+
+function Star() {
+    console.log('Star rendering')
+    return (
+        <div>star</div>
+    )
+
+}
