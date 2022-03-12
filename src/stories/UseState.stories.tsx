2cf3f779f16a2fec9f7f3e98e179cbcfd142f296
TichonovRoman <romikt@inbox.ru>
2022-03-12 20:19:04 +0300

    22: useeffect

--- src/stories/UseState.stories.tsx
+++ src/stories/UseState.stories.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 
 export default {
@@ -7,20 +7,66 @@ export default {
 
 function generation() {
     console.log("generation")
-    return 4345466
+    return 1
 }
 
 export const Example1 = () => {
     console.log("Example1")
 
+    const changeCount = (state: number) => state + 1
+
+
     const [counter, setCounter] = useState(generation)
 
-    const changeCount = (state: number) => {
-        return state + 1
-    }
+    useEffect(()=>{
+        console.log("useEffect срабатывает всегда")
+        document.title = counter.toString()
+    })
+    useEffect(()=>{
+        console.log("useEffect срабатывает только при первой отрисовке")
+        document.title = counter.toString()
+    }, [])
+    useEffect(()=>{
+        console.log("useEffect срабатывает при 1 отрисовке и каждый раз при изменении counter")
+        document.title = counter.toString()
+    }, [counter])
+
+
+
+
+
+
 
     return <>
     <button onClick={()=>setCounter(changeCount)}>+</button>
         {counter}
+    </>
+}
+export const SetTimeoutExample = () => {
+    console.log("SetTimeoutExample")
+    const [counter, setCounter] = useState(1)
+
+
+    const changeCount = (state: number) => state + 1
+
+    const [hours, setHours] = useState(0)
+    const [minuts, setMinuts] = useState(0)
+    const [seconds, setSeconds] = useState(0)
+
+    useEffect(()=>{
+        setInterval(() => {
+            let a = new Date()
+            setHours(a.getHours())
+            setMinuts(a.getMinutes())
+            setSeconds(a.getSeconds())
+           setCounter((state) => state + 1)
+        }, 1000)
+    }, [])
+
+
+
+    return <>
+    <button onClick={()=>setCounter(changeCount)}>+</button>
+        {hours}:{minuts}:{seconds}
     </>
 }
